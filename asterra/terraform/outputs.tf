e589a3fc86089f08ad6dbe872b8551021ad3eb9e
Yakir Adela <yakirade@gmail.com>
2025-08-03 11:03:00 +0300

    Update outputs.tf

--- asterra/terraform/outputs.tf
+++ asterra/terraform/outputs.tf
@@ -15,3 +15,23 @@ output "s3_bucket_name" {
 output "aws_ecr_repository_url" {
   value       = aws_ecr_repository.ecr-repo.repository_url
 }
+
+output "eks_cluster_endpoint" {
+  description = "The endpoint URL of the EKS cluster"
+  value       = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = module.eks.cluster_name
+}
+
+output "eks_cluster_security_group_id" {
+  description = "The security group ID attached to the EKS cluster"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "eks_node_group_role_arn" {
+  description = "IAM Role ARN for the EKS node group"
+  value       = module.eks.node_groups_iam_role_arns["default"]
+}
