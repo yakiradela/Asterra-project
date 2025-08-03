2b4ebd387960e663d7d06ab1e8c177a7affe8eda
Yakir Adela <yakirade@gmail.com>
2025-08-03 12:19:56 +0300

    Update main.tf

--- asterra/terraform/main.tf
+++ asterra/terraform/main.tf
@@ -77,6 +77,31 @@ resource "aws_ecr_repository" "ecr-repo" {
   name = "${var.project_name}-repo"
 }
 
+resource "aws_security_group" "eks_sg" {
+  name        = "${var.project_name}-eks-sg"
+  description = "Allow HTTPS inbound traffic to EKS"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTPS traffic"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-eks-sg"
+  }
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = ">= 21.0.0"
