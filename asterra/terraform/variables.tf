a167c94b915682b326f4b7d2ab93964f70d82e34
Yakir Adela <yakirade@gmail.com>
2025-08-04 20:44:46 +0300

    Update variables.tf

--- asterra/terraform/variables.tf
+++ asterra/terraform/variables.tf
@@ -55,5 +55,5 @@ variable "environment" {
 variable "suffix" {
   description = "A unique suffix to avoid naming collisions"
   type        = string
-  default     = "dev01"
+  default     = "0001"
 }
